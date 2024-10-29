c5512ebab72cdcfd1a82187a239ead5a8abe60bf
franciscoIt <ivnro.dev@gmail.com>
2024-10-29 01:46:54 +0000

    initial c

---
+++ backend/core/config.py
@@ -0,0 +1,21 @@
+import os 
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+print(os.getenv("DEBUG"))
+class Settings:
+    PROJECT_NAME:str = "Mondongo"
+    PROJECT_VERSION: str = "1.0.0"
+
+    DATABASE_USER : str = os.getenv("DATABASE_USER")
+    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+    DATABASE_SERVER : str = os.getenv("DATABASE_SERVER","localhost")
+    DATABASE_PORT : str = os.getenv("DATABASE_PORT",5432) # default POSTGRES port is 5432
+    DATABASE_DB : str = os.getenv("DATABASE_DB","tdd")
+    # DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_SERVER}:{DATABASE_PORT}/{DATABASE_DB}"
+    if os.getenv("ENVIRONMENT") == "development":
+        DATABASE_URL = "sqlite:///./sql_app.db"
+
+settings = Settings()
---
+++ backend/db/base_class.py
@@ -0,0 +1,41 @@
+from typing import Any
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import as_declarative
+
+"""
+With @as_declarative, SQLAlchemy will:
+
+
+    - Recognize Base as the base class for all models 
+    (i.e., any class inheriting from Base will 
+    automatically have SQLAlchemy ORM features).
+
+    - Enable configuration of common functionality 
+    across models, like automatically generating 
+    table names (as shown in your code) or defining 
+    shared columns and methods."""
+
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+
+    #to generate tablename from classname
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+"""
+The @declared_attr decorator is applied to a method 
+or attribute in a base class (like Base). 
+It indicates that this attribute should be evaluated 
+each time a subclass is created. Here, it is used to 
+define the __tablename__ attribute, which SQLAlchemy 
+uses to name tables in the database.
+"""
+
+"""
+In this code, @declared_attr allows __tablename__ 
+to be set dynamically based on the subclass name 
+(cls.__name__.lower()). For example:
+*** Removes the need to manually specify __tablename__ for every model.
+"""
